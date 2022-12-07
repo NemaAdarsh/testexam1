f5a90d16003efc186f404f7f1281b5266e696476
Adaddy1 <88324861+Adaddy1@users.noreply.github.com>
2022-12-07 12:39:11 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,15 +1,1 @@
-numbers = [1, 2, 3, 4, 5, 6]
-target = 3
-
-result = linear_search(numbers, target)
-
-if result == -1:
-    print("Target not found in list.")
-else:
-    print("Target found at index:", result)
-def linear_search(list, target):
-    for i in range(len(list)):
-        if list[i] == target:
-            return i
-
-    return -1
+print("sometihng")
